e1d0fc8f8721cd92eb8e9347496026701d0ba4b2
janithgunawardana1998 <janithgunawardana98@gmail.com>
2023-10-15 02:32:54 +0530

    New login

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 import {name as appName} from './app.json';
-import AppPro from './AppPro';
+import AppPro from './src/screens/login/Index';
 AppRegistry.registerComponent(appName, () => AppPro);
